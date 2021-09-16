73e924f79d19f970d64046cc802f9860ff22aaec
Dimuth Ruwantha <dimuth@CreativeSoftware.com>
2021-09-16 07:00:03 +0530

    process all web requests

--- Services/CocktailService.cs
+++ Services/CocktailService.cs
@@ -16,16 +16,42 @@ namespace Services
         }
         public async Task<CocktailList> SearchCockTail(string ingredient)
         {
+            var cocktailsRaw = await _http.GetFromJsonAsync<CockTails>($"api/json/v1/1/filter.php?i=Gin");
             
-            var c = await _http.GetFromJsonAsync<CockTails>($"api/json/v1/1/filter.php?i=Gin");
+            if (cocktailsRaw == null) return null;
+            var cocktails = await FillMetaData(cocktailsRaw);
             
-            return new CocktailList();
+            return cocktails;
         }
 
         public async Task<Cocktail> GetRandomCockTail()
         {
             var c = await _http.GetFromJsonAsync<CockTails>($"/api/json/v1/1/random.php");
             return new Cocktail();
+        }
+
+        private async Task<CocktailList> FillMetaData(CockTails cocktails)
+        {
+            var cockTailList = new CocktailList()
+            {
+                meta = new ListMeta()
+                {
+                    firstId = int.MaxValue,
+                    lastId = int.MinValue,
+                    count = cocktails.Drinks.Count,
+                }
+            };
+           
+            foreach (var drink in cocktails.Drinks)
+            {
+                //https://stackoverflow.com/questions/15136542/parallel-foreach-with-asynchronous-lambda
+                var drinkId = int.Parse(drink.idDrink);
+                if(drinkId < cockTailList.meta.firstId) cockTailList.meta.firstId = drinkId;
+                if(drinkId > cockTailList.meta.lastId) cockTailList.meta.lastId = drinkId;
+                var details = await _http.GetFromJsonAsync<CockTails>($"api/json/v1/1/lookup.php?i={drink.idDrink}");
+            }
+
+            return cockTailList;
         }
     }
 }
